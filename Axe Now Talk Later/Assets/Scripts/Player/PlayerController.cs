8ec4a6374283be50e848b454682e4ce788d10e30
MINH BO <minhbo0000@gmail.com>
2018-06-09 12:04:03 -0800

    restructure project

---
+++ Axe Now Talk Later/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * CREATED by MINH
+ * SUMMARY: button inputs mapping and listener
+ */
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            print("Move Up");
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            print("Move Down");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            print("Move Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Move Right");
+        }
+
+        //TODO: 
+    }
+}
